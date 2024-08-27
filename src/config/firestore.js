6430852c8c196b2566a20c56b66999d64dc26d18
Sameer Ahmed <sameer.shahzad@systemsltd.com>
2024-08-27 17:04:04 +0500

    first commit

---
+++ src/config/firestore.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtVmqkfMzNQISOmWA05kNPe3ghsZf2C_Y",
+  authDomain: "frontend-e1581.firebaseapp.com",
+  projectId: "frontend-e1581",
+  storageBucket: "frontend-e1581.appspot.com",
+  messagingSenderId: "1065932053713",
+  appId: "1:1065932053713:web:d927a9326623f3835579f0",
+  measurementId: "G-Q1CJ82RYVJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
