7f56e1eb8a1e0fc308a58a78b793c147d55f3281
Sameer959 <sameerahmd385@gmail.com>
2024-08-27 17:04:57 +0500

    Update firestore.js

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtVmqkfMzNQISOmWA05kNPe3ghsZf2C_Y",
-  authDomain: "frontend-e1581.firebaseapp.com",
-  projectId: "frontend-e1581",
-  storageBucket: "frontend-e1581.appspot.com",
-  messagingSenderId: "1065932053713",
-  appId: "1:1065932053713:web:d927a9326623f3835579f0",
-  measurementId: "G-Q1CJ82RYVJ"
+  apiKey: api key id,
+  authDomain: aut domain id,
+  projectId: project id,
+  storageBucket: storage bucket id,
+  messagingSenderId: messaging sender id,
+  appId: "1:1065932053713:web:app id,
+  measurementId: measurement id,
 };
 
 const app = initializeApp(firebaseConfig);
